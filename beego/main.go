699222b1b1f056b5d6c8d180f8e6d4d028e2bfac
cheyuexian <694547990@qq.com>
2020-09-07 00:37:08 +0800

    beego

--- beego/main.go
+++ beego/main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func init() {
+	orm.RegisterDriver("mysql", orm.DRMySQL)
+
+	orm.RegisterDataBase("default", "mysql", "root:123456@(192.168.177.132:3306)/orm_test?charset=utf8")
+}
+
 func main() {
-	
+	o := orm.NewOrm()
+	o.Using("default") // 默认使用 default，你可以指定为其他数据库
+	orm.RunSyncdb("default", false, true)
+	orm.Debug = true
+	profile := new(Profile)
+	profile.Age = 30
+
+	user := new(User)
+	user.Profile = profile
+	user.Name = "slene"
+
+	fmt.Println(o.Insert(profile))
+	fmt.Println(o.Insert(user))
 }
